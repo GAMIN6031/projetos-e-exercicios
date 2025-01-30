48c7909a21a3825c7f04324d9d54a0b76302611a
GAMIN6031 <pedrocorreajogo@gmail.com>
2025-01-30 18:35:44 -0300

    ok

--- exercicios copy/ex003/index.js
+++
@@ -1,7 +0,0 @@
-
-// Se 'A' for igual a 'b' ele vai somar e se for diferente ele vai muntiplicar
-
-let A = 3
-let B = 4
-let C = (A == B ? A + B : A * B);
-console.log(C)
--- exercicios copy/ex004/index.js
+++
@@ -1,5 +0,0 @@
-
-// Vai pegar um valor inteiro e escrever o seu sucessor e antecessor
-
-let A = 5
-console.log("Valor: " + A, " | Seu antecessor: " + (A - 1) , " | Seu sucessor: " + (A + 1));
